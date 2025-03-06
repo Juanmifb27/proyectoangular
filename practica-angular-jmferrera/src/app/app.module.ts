0bdb0ea2bb1ffd63c0a6ef325e034f17e639d887
juanmifb27-admin <juanmanuel.ferrera-berlanga@iesruizgijon.com>
2025-03-06 21:40:57 +0100

    solucionado,  listaJuegos no cargaba

--- practica-angular-jmferrera/src/app/app.module.ts
+++ practica-angular-jmferrera/src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { ListadojuegosComponent } from './listadojuegos/listadojuegos.component';
 import { DetallesJuegoComponent } from './detalles-juego/detalles-juego.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListadojuegosComponent,
     DetallesJuegoComponent,
-    HomeComponent
+    HomeComponent,
+    Pagina404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
